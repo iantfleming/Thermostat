3ae80166d877bfe2084929003d3b33b60c42298b
Ian Fleming <>
2021-01-14 15:28:36 +0000

    up and down

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,5 +8,18 @@ describe('Thermostat', function() {
   it('starts at 20 degrees', function(){
     expect(thermostat.temperature).toEqual(20);
   });
-  
+
+  describe('up', function(){
+    it('increases the temperature', function(){
+      thermostat.up();
+      expect(thermostat.getCurrentTemperature()).toEqual(21);
+    });
+  });
+
+  describe('down', function(){
+    it('decreases the temperature', function(){
+      thermostat.down();
+      expect(thermostat.getCurrentTemperature()).toEqual(19);
+    });
+  });
 });
